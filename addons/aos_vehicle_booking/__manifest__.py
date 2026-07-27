0ffeec30ebe780522ec7597a586f48d507c0901f
Ao Watchara <48900683+aocool9509@users.noreply.github.com>
2026-07-27 12:22:42 +0700

    feat(vehicle-booking): add initial security, menus and views

--- addons/aos_vehicle_booking/__manifest__.py
+++ addons/aos_vehicle_booking/__manifest__.py
@@ -6,7 +6,11 @@
     "license": "LGPL-3",
     "depends": ["base", "aos_base"],
     "data": [
+        "security/ir.model.access.csv",
         "data/vehicle_booking_sequence.xml",
+        "views/vehicle_views.xml",
+        "views/vehicle_booking_views.xml",
+        "views/vehicle_booking_menus.xml",
     ],
     "installable": True,
     "application": True,
